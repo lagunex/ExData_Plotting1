87defb24d0d81d601a1db909047e68c1fc339f4e
Carlos A. Henríquez Q <carlos.henriquez@lagunex.com>
2015-03-04 13:57:18 +0100

    add data parameter to plotX() functions

--- plot1.R
+++ plot1.R
@@ -2,8 +2,10 @@ source("read_data.R")
 
 ##
 # Plot a histogram in a PNG file called plot1.png
-plot1 <- function() {
-    data <- read_data()
+#
+# data - dataframe used for plotting
+plot1 <- function(data = NULL) {
+    data <- if (is.null(data)) { read_data() } else { data }
     png(filename = "plot1.png", width = 480, height = 480)
     hist(data$Global_active_power,
          col="red",
--- plot4.R
+++ plot4.R
@@ -5,7 +5,7 @@ source("plot3.R")
 ##
 # Plot 2 different line graphs given the data
 #
-# data - dataframe to use for plotting
+# data - dataframe used for plotting
 plotOthers <- function(data) {
     with(data, {
         xdata <- seq_along(Date)
@@ -21,9 +21,10 @@ plotOthers <- function(data) {
 ##
 # Plots 4 different graph in a 2x2 plot matrix
 #
+# data - dataframe used for plotting
 # toPng - indicates if the result must be saved in a file called plot4.png
-plot4 <- function(toPng = TRUE) {
-    data <- read_data()
+plot4 <- function(data = NULL, toPng = TRUE) {
+    data <- if (is.null(data)) { read_data() } else { data }
 
     if (toPng) {
         png(filename = "plot4.png", width = 480, height = 480)
